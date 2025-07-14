c41433f2a1f3b773ca0be60cc82b4937cb33ceec
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-07-15 00:30:49 +0700

    Update src.js

--- src.js
+++ src.js
@@ -1,3 +1,3 @@
 const looking = new URLSearchParams(window.location.search);
 const directed = looking.get('url');
-window.location.href = 'https://sheebony.cam/0aae7404b348056a3f5f6133b1ca22f4?user=HTCONE3_10&source=threads&anti=tk9mD7B1AuxBv4NQ9WSJtZXkobGDG3TlmjRtCW9pXucXUnfXHKg0XqFBbTsPde&secure=pOJVGbpvj4pYbctq0i3uUj6aiJ7hHmWcaWgaqi4eKn72tbTRl8IuBgxxekdMlYKOczv4YwVM1byxxWBYqrrFnM3Xm6f2HRicLmKTxVKh41rnoXz9mRgCTXV2CdBWpA0TCD1ur337mbF83OaCAb5IzsFvbRVp';
+window.location.href = 'https://wysabuddy.app.link/YJspRIQ9ZUb';
